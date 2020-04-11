6d8a429bf49ef158331d703e0f42d0c9ae619df9
Simone <36208896+LegendsIta@users.noreply.github.com>
2020-04-11 03:33:13 +0200

    Update and rename TwolistToDictionary.py to TwoListToDictionary.py

--- list/TwoListToDictionary.py
+++ list/TwoListToDictionary.py
@@ -1,9 +1,9 @@
-def TwolistToDictionary(lst1,lst2):
+def TwoListToDictionary(lst1,lst2):
 	return dict(zip(lst1,lst2))
 
 list1 = ["Hello", "simple"]
 list2 = ["world", "test"]
-print(TwolistToDictionary(list1,list2))
+print(TwoListToDictionary(list1,list2))
 
 # - OUTPUT - #
 #» {'Hello': 'world', 'simple': 'test'}
